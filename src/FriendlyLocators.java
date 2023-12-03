e850a8dde2edc9335160ede2e947f6aee31ea5ab
Amit <amitkumar225176@gmail.com>
2023-12-03 15:37:18 +0530

    FriendlyLocators file is added and notes file is updated

--- src/FriendlyLocators.java
+++ src/FriendlyLocators.java
@@ -1,8 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.openqa.selenium.support.locators.RelativeLocator.*;  //By default selenium will not autosuggest static package
+
+import org.openqa.selenium.By;
 
 public class FriendlyLocators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		System.setProperty(webdriver.chrome.driver, "path");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		
+//		above
+		WebElement editBox = driver.findElement(By.xpath("//input[@name=\"name\"]"));
+		driver.findElement(with(By.tagName("label")).above(editBox));
+		
+//		below
+		WebElement gender = driver.findElement(By.xpath("//label[@for='exampleFormControlSelect1']"));
+		driver.findElement(with(By.tagName("select")).below(gender));
+		
+//		left
+		WebElement iceCreamLabel =driver.findElement(By.xpath("//label[text()='Check me out if you Love IceCreams!']"));
+		driver.findElement(with(By.tagName("input")).toLeftOf(iceCreamLabel)).click();
+
+//		right
+		WebElement rdb = driver.findElement(By.id("inlineRadio1"));
+		System.out.println(driver.findElement(with(By.tagName("label")).toRightOf(rdb)).getText());
+		
 
 	}
 
